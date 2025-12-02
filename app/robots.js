3c8aa6ef52c86e6c86b85ea852dce74fd35b039c
Linus Nicolau <linusnicolau@protonmail.com>
2025-12-02 13:33:35 +0100

    optimizacion

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
+        sitemap: 'https://www.albertfreixa.com', // Cambia por tu dominio real
+    }
+}
